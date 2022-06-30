c6f2e4d4cb7434c1d00a5913f2eca582818628f3
Diego <dpardoblacutt@gmail.com>
2022-06-30 18:17:33 -0400

    Change hardcoded port and use env variable instead

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const express = require('express');
 
 const app = express();
@@ -6,6 +8,6 @@ app.get("/",(req,res)=>{
     res.json({mssg:"Welcome to the app"})
 })
 
-app.listen(4000, () => {
-    console.log("listening on port 4000");
+app.listen(process.env.PORT, () => {
+    console.log("listening on port",process.env.PORT);
 })
