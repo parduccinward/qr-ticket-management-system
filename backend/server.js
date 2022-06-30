6fe7048cce00ef75509c3a28bd3fa6325b4a40e9
Diego <dpardoblacutt@gmail.com>
2022-06-30 18:06:34 -0400

    Add route with get request and home route that sends a message

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.json({mssg:"Welcome to the app"})
+})
+
 app.listen(4000, () => {
     console.log("listening on port 4000");
 })
