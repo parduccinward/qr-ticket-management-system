d2a9a7ea55643ca090ae3131dc04c3b6982d784b
Diego <dpardoblacutt@gmail.com>
2022-07-01 18:17:32 -0400

    Add CRUD routes inside salesperson.js

---
+++ backend/routes/salespersons.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/",(req,res) => {
+    res.json({mssg:"GET all salespersons"});
+})
+
+router.get("/:id", (req,res) => {
+    res.json({mssg: "GET a single salesperson"});
+})
+
+router.post("/",(req,res) => {
+    res.json({mssg:"POST a new salesperson"});
+})
+
+router.delete("/:id", (req,res) => {
+    res.json({mssg: "DELETE a salesperson"});
+})
+
+router.patch("/:id", (req,res) =>{
+    res.json({mssg: "UPDATE a salesperson"});
+})
+
+module.exports = router;
