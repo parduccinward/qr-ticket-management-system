054aef25ff9822119d59eca644c035688ea084bd
Diego <dpardoblacutt@gmail.com>
2022-07-01 17:45:25 -0400

    Add routes to create, read, update and delete parties

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -2,7 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/', () => {})
+router.get("/", (req, res) => {
+    res.json({mssg: "GET all parties"});
+})
+
+router.get("/:id", (req, res) => {
+    res.json({mssg: "GET a single party"});
+})
+
+router.post("/", (req, res) => {
+    res.json({mssg: "POST a new party"})
+})
+
+router.delete("/:id", (req, res) => {
+    res.json({mssg: "DELETE a party"});
+})
+
+router.patch("/:id", (req, res) => {
+    res.json({mssg: "UPDATE a party"})
+})
 
 
 module.exports = router;
