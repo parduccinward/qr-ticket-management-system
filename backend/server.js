89d2a1e798a10e2f2bcfe6d8efe9bf2fd52f7851
Diego <dpardoblacutt@gmail.com>
2022-07-01 18:29:36 -0400

    Add clients routes inside server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const express = require('express');
 const partyRoutes = require("./routes/parties");
 const salespersonRoutes = require("./routes/salespersons");
+const clientRoutes = require("./routes/clients");
 
 
 
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 app.use("/api/parties",partyRoutes);
 app.use("/api/salespersons", salespersonRoutes);
+app.use("/api/clients",clientRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log("listening on port",process.env.PORT);
