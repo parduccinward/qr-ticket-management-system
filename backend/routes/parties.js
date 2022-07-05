5a4dea029dffce1f1f01f5a1f19e0aab1f38c178
Diego <dpardoblacutt@gmail.com>
2022-07-05 11:05:19 -0400

    Create insert query inside party post route

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -11,8 +11,17 @@ router.get("/:id", (req, res) => {
     res.json({mssg: "GET a single party"});
 })
 
-router.post("/", (req, res) => {
-    res.json({mssg: "POST a new party"})
+router.post("/", async (req, res) => {
+    try {
+        const {name, sale_start_date, sale_end_date, party_date, banner_url} = req.body;
+        const newParty = await pool.query(
+            "INSERT INTO parties (name, sale_start_date, sale_end_date, party_date, banner_url) VALUES($1, $2, $3, $4, $5) RETURNING *",
+             [name, sale_start_date, sale_end_date, party_date, banner_url]
+             );
+        res.json(newParty);
+    } catch (err) {
+        console.error(err.message);
+    }
 })
 
 router.delete("/:id", (req, res) => {
