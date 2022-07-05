cd08ff02353e9f9fb4c8dfe138638a856579c30e
Diego <dpardoblacutt@gmail.com>
2022-07-05 19:16:42 -0400

    Separate routes and controllers

---
+++ backend/controllers/partyController.js
@@ -0,0 +1,66 @@
+const pool = require("../models/db");
+
+const getParties = async (req, res) => {
+    try {
+        const allParties = await pool.query("SELECT * FROM parties");
+        res.json(allParties.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+const getParty = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const party = await pool.query("SELECT * FROM parties WHERE party_id = $1 ",[id]);
+        res.json(party.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+const createParty = async (req, res) => {
+    try {
+        const {name, sale_start_date, sale_end_date, party_date, banner_url} = req.body;
+        const newParty = await pool.query(
+            "INSERT INTO parties (name, sale_start_date, sale_end_date, party_date, banner_url) VALUES($1, $2, $3, $4, $5) RETURNING *",
+             [name, sale_start_date, sale_end_date, party_date, banner_url]
+             );
+        res.json(newParty.rows);
+    } catch (err) {
+        console.error(err.message); 
+    }
+}
+
+const deleteParty = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteParty = await pool.query("DELETE FROM parties WHERE party_id =$1",[id]);
+        res.json("Party was deleted successfully!")
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+const updateParty = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {name, sale_start_date, sale_end_date, party_date, banner_url} = req.body;
+        const updateParty = await pool.query(
+            "UPDATE parties SET name=$1, sale_start_date=$2, sale_end_date=$3, party_date=$4, banner_url=$5 WHERE party_id=$6",
+             [name, sale_start_date, sale_end_date, party_date, banner_url, id]
+             );
+        res.json("Party was updated successfully");
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+
+module.exports = {
+    getParties,
+    getParty,
+    createParty,
+    deleteParty,
+    updateParty
+}
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -1,63 +1,24 @@
 const express = require("express");
-const pool = require("../models/db");
+
+const {
+    getParties,
+    getParty,
+    createParty,
+    deleteParty,
+    updateParty
+} = require("../controllers/partyController");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        const allParties = await pool.query("SELECT * FROM parties");
-        res.json(allParties.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+router.get("/",getParties);
 
-router.get("/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const party = await pool.query("SELECT * FROM parties WHERE party_id = $1 ",[id]);
-        res.json(party.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+router.get("/:id",getParty);
 
-router.post("/", async (req, res) => {
-    try {
-        const {name, sale_start_date, sale_end_date, party_date, banner_url} = req.body;
-        const newParty = await pool.query(
-            "INSERT INTO parties (name, sale_start_date, sale_end_date, party_date, banner_url) VALUES($1, $2, $3, $4, $5) RETURNING *",
-             [name, sale_start_date, sale_end_date, party_date, banner_url]
-             );
-        res.json(newParty.rows);
-    } catch (err) {
-        console.error(err.message); 
-    }
-})
+router.post("/", createParty);
 
-router.delete("/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const deleteParty = await pool.query("DELETE FROM parties WHERE party_id =$1",[id]);
-        res.json("Party was deleted successfully!")
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+router.delete("/:id",deleteParty);
 
-router.put("/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const {name, sale_start_date, sale_end_date, party_date, banner_url} = req.body;
-        const updateParty = await pool.query(
-            "UPDATE parties SET name=$1, sale_start_date=$2, sale_end_date=$3, party_date=$4, banner_url=$5 WHERE party_id=$6",
-             [name, sale_start_date, sale_end_date, party_date, banner_url, id]
-             );
-        res.json("Party was updated successfully");
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+router.put("/:id",updateParty);
 
 
 module.exports = router;
