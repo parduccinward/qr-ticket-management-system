d658f65dc9064be204390fceed2a9a49acabb9df
Diego <dpardoblacutt@gmail.com>
2022-07-06 18:48:03 -0400

    Connect user routes to main app

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const partyRoutes = require("./routes/parties");
 const salespersonRoutes = require("./routes/salespersons");
 const clientRoutes = require("./routes/clients");
+const userRoutes = require("./routes/users");
 
 
 const app = express();
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 app.use("/api/parties",partyRoutes);
 app.use("/api/salespersons", salespersonRoutes);
 app.use("/api/clients",clientRoutes);
+app.use("/api/users",userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log("listening on port",process.env.PORT);
