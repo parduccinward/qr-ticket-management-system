6734a9bc1f5b3bd0db958ef3900188feb9f940fa
Diego <dpardoblacutt@gmail.com>
2022-07-06 20:16:33 -0400

    Create register route inside users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
-const {registerUser} = require("../controllers/signupController");
-const {loginUser} = require("../controllers/loginController");
-
+const registerUser = require("../controllers/signupController");
+const loginUser = require("../controllers/loginController");
 
 const router = express.Router();
+
+router.post("/register", registerUser);
+
+// router.post("/login", loginUser);
 
 module.exports = router;
