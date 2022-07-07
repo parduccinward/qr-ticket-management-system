046feb12f554c24d38b4da7ace6b24be54bdf0ed
Diego <dpardoblacutt@gmail.com>
2022-07-07 19:05:04 -0400

    Add browserRouter, routes and a home route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className ="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
