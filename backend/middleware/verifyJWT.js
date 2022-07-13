f04f605058944fea2ef303a69badbfbf67c8fff8
Diego <dpardoblacutt@gmail.com>
2022-07-13 15:58:28 -0400

    Create verifyJWT middleware that validates token in each server request

---
+++ backend/middleware/verifyJWT.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token =authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, user) => {
+            if (err) return res.sendStatus(403);
+            req.user = user;
+            next();
+        }
+    );
+}
+
+module.exports = verifyJWT;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 
 const express = require('express');
 const cors = require('cors');
+const verifyJWT = require('./middleware/verifyJWT');
 const partyRoutes = require("./routes/parties");
 const salespersonRoutes = require("./routes/salespersons");
 const clientRoutes = require("./routes/clients");
@@ -22,6 +23,8 @@ app.use("/api/parties",partyRoutes);
 app.use("/api/salespersons", salespersonRoutes);
 app.use("/api/clients",clientRoutes);
 app.use("/api/auth",userRoutes);
+
+app.use(verifyJWT);
 
 app.listen(process.env.PORT, () => {
     console.log("listening on port",process.env.PORT);
