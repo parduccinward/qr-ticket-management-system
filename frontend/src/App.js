25a2dc00dc2496a33c126dc846750f2d5258b923
Diego <dpardoblacutt@gmail.com>
2022-07-14 18:00:49 -0400

    Change login be inside App instead of navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-// import Login from './components/login/Login';
-import Navbar from "./components/navbar/Navbar"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import Parties from "./pages/Parties";
-import Salespersons from "./pages/Salespersons";
-import Clients from "./pages/Clients";
+import Login from './components/login/Login';
 
 function App() {
   return (
     <main className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/parties" element={<Parties/>}/>
-          <Route path="/salespersons" element={<Salespersons/>}/>
-          <Route path="/clients" element={<Clients/>}/>
-        </Routes>
-      </Router>
+      <Login/>
     </main>
   );
 }
