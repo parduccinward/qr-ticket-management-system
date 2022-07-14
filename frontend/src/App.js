574ba6c4f859d660e8faca1cc070c5c6df0b9a90
Diego <dpardoblacutt@gmail.com>
2022-07-14 16:55:19 -0400

    Add link to different components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 // import Login from './components/login/Login';
 import Navbar from "./components/navbar/Navbar"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Parties from "./pages/Parties";
+import Salespersons from "./pages/Salespersons";
+import Clients from "./pages/Clients";
 
 function App() {
   return (
@@ -8,7 +12,10 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/"/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/parties" element={<Parties/>}/>
+          <Route path="/salespersons" element={<Salespersons/>}/>
+          <Route path="/clients" element={<Clients/>}/>
         </Routes>
       </Router>
     </main>
