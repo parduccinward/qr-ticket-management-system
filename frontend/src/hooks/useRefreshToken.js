fbf856dfee64468d2b0a90ccd9493b8a7f48a776
Diego <dpardoblacutt@gmail.com>
2022-07-25 16:31:50 -0400

    Remove console logs on RefreshToken

--- frontend/src/hooks/useRefreshToken.js
+++ frontend/src/hooks/useRefreshToken.js
@@ -8,8 +8,6 @@ const useRefreshToken = () => {
             withCredentials: true
         });
         setAuth(prev => {
-            console.log(JSON.stringify(prev));
-            console.log(response.data.accessToken);
             return { ...prev, accessToken: response.data.accessToken }
         });
         return response.data.accessToken;
