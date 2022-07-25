237b11d78b1de4ffa38f291ea12b5dd8a6670d0b
Diego <dpardoblacutt@gmail.com>
2022-07-25 18:25:26 -0400

    Update number of salesperson fields in backend adding party_id

--- backend/controllers/salespersonController.js
+++ backend/controllers/salespersonController.js
@@ -21,10 +21,10 @@ const getSalesperson = async (req, res) => {
 
 const createSalesperson = async (req, res) => {
     try {
-        const {name,last_name, phone, email, sale_url} = req.body;
+        const {name,last_name, phone, email, sale_url, party_id} = req.body;
         const newSalesperson = await pool.query(
-            "INSERT INTO salespersons (name, last_name, phone, email, sale_url) VALUES($1, $2, $3, $4, $5) RETURNING *",
-             [name,last_name, phone, email, sale_url]
+            "INSERT INTO salespersons (name, last_name, phone, email, sale_url, party_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+             [name,last_name, phone, email, sale_url, party_id]
              );
         res.json(newSalesperson.rows);
     } catch (err) {
@@ -45,10 +45,10 @@ const deleteSalesperson = async (req, res) => {
 const updateSalesperson = async (req, res) => {
     try {
         const {id} = req.params;
-        const {name,last_name, phone, email, sale_url} = req.body;
+        const {name,last_name, phone, email, sale_url, party_id} = req.body;
         const updateSalesperson = await pool.query(
-            "UPDATE salespersons SET name = $1, last_name = $2, phone = $3, email = $4, sale_url = $5 WHERE salesperson_id = $6",
-             [name,last_name, phone, email, sale_url, id]
+            "UPDATE salespersons SET name = $1, last_name = $2, phone = $3, email = $4, sale_url = $5, party_id = $6 WHERE salesperson_id = $7",
+             [name,last_name, phone, email, sale_url, party_id, id]
              );
         res.json("Salesperson was updated successfully");
     } catch (err) {
