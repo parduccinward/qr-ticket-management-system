0fd43e665bd60700a305de19e72ae3ef0437783f
Diego <dpardoblacutt@gmail.com>
2022-07-31 12:42:16 -0400

    Remove save image in server upload folder, instead upload to Cloudinary directly

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -4,11 +4,7 @@ const multer = require("multer");
 const path = require("path")
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb) => {
-        cb(null, "./uploads")
-    },
     filename: (req, file, cb) => {
-        console.log(file)
         cb(null, Date.now() + path.extname(file.originalname))
     }
 })
